77615f9c87e11f152043bcfc3ab993dc73bd642c
hgeyer04 <56579343+hgeyer04@users.noreply.github.com>
2020-05-04 15:06:06 +0200

    noch render probleme

--- src/app/components/content/view/calendar/calendar.component.ts
+++ src/app/components/content/view/calendar/calendar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
 
 @Component({
   selector: 'app-calendar',
@@ -6,8 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
+  calendar: Calendar;
 
-  constructor() { }
+  constructor() { 
+    let calendarElement = document.getElementById('calendar');
+    this.calendar = new Calendar(calendarElement, {
+      plugins: [ dayGridPlugin, timeGridPlugin, listPlugin]
+    });
+    this.calendar.render();
+  }
 
   ngOnInit(): void {
   }
