bc9c1621ddf65e2f01a034a5392389c1820d3a8a
Georg Burkl <gburkl@student.tgm.ac.at>
2020-05-10 00:33:16 +0200

    add event changed callback
    Signed-off-by: Minecraftschurli <minecraftschurli@gmail.com>

--- src/app/components/content/view/calendar/calendar.component.ts
+++ src/app/components/content/view/calendar/calendar.component.ts
@@ -10,6 +10,7 @@ import deLocale from '@fullcalendar/core/locales/de';
 import {FullCalendarComponent} from '@fullcalendar/angular';
 import {environment} from '../../../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
+import {Duration} from '@fullcalendar/core/datelib/duration';
 
 @Component({
   selector: 'app-calendar',
@@ -22,7 +23,7 @@ export class CalendarComponent implements AfterViewInit {
   private id = '57162059-91d4-11ea-9fd5-5048494f4e43'; // todo add calendar id
 
   constructor(private readonly http: HttpClient) {
-    this.calendarOptions = new Options(this.eventClicked);
+    this.calendarOptions = new Options(this.eventClicked, this.eventChanged);
   }
 
   ngAfterViewInit(): void {
@@ -41,31 +42,69 @@ export class CalendarComponent implements AfterViewInit {
     return this.calendarComponent.getApi();
   }
 
-  eventClicked(event) {
-    // event click handeln
+  eventClicked(event: EventApi) {
+    // event click handling
+  }
+
+  eventChanged(arg: EventChangedInfo) {
+    // event change handling
   }
 }
 
-type EventInfo = {
+type EventClickInfo = {
   el: HTMLElement;
   event: EventApi;
   jsEvent: MouseEvent;
   view: ViewApi;
 };
 
+type EventResizeInfo = {
+  el: HTMLElement;
+  startDelta: Duration;
+  endDelta: Duration;
+  prevEvent: EventApi;
+  event: EventApi;
+  revert: () => void;
+  jsEvent: Event;
+  view: ViewApi
+};
+
+type EventDropInfo = {
+  el: HTMLElement;
+  event: EventApi;
+  oldEvent: EventApi;
+  delta: Duration;
+  revert: () => void;
+  jsEvent: Event;
+  view: ViewApi
+};
+
+type EventChangedInfo = {
+  el: HTMLElement,
+  oldEvent: EventApi,
+  newEvent: EventApi,
+  revert: () => void,
+  jsEvent: Event,
+  view: ViewApi
+};
+
 /**
  * The options for the calendar
  */
 class Options implements OptionsInput {
-  private readonly eventCallback: (event) => void;
+  private readonly eventClickCallback: (event: EventApi) => void;
+  private readonly eventChangeCallback: (arg: EventChangedInfo) => void;
 
-  constructor(eventCallback: (event) => void) {
-    this.eventCallback = eventCallback;
+  constructor(eventClickCallback: (event: EventApi) => void,
+              eventChangeCallback: (arg: EventChangedInfo) => void
+  ) {
+    this.eventClickCallback = eventClickCallback;
+    this.eventChangeCallback = eventChangeCallback;
   }
 
   height: 'parent';
   contentHeight: 'auto';
-  aspectRation = 1;
+  aspectRatio = 1;
 
   // locale
   locales = [ deLocale ];
@@ -91,9 +130,19 @@ class Options implements OptionsInput {
   selectable = true;
   editable = true;
 
-  eventClick(info: EventInfo) {
+  eventClick(info: EventClickInfo) {
     info.jsEvent.preventDefault();
-    this.eventCallback(info.event);
+    this.eventClickCallback(info.event);
+  }
+
+  eventDrop(info: EventDropInfo) {
+    this.eventChangeCallback(
+      {el: info.el, oldEvent: info.oldEvent, newEvent: info.event, revert: info.revert, jsEvent: info.jsEvent, view: info.view});
+  }
+
+  eventResize(info: EventResizeInfo) {
+    this.eventChangeCallback(
+      {el: info.el, oldEvent: info.prevEvent, newEvent: info.event, revert: info.revert, jsEvent: info.jsEvent, view: info.view});
   }
 }
 
